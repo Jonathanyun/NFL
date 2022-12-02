f7b854ffc82bea6e0bb9ed2650b2a6a705feac4f
Jonathan <sukwonyun@protonmail.com>
2022-12-01 23:53:40 -0600

    Changed from python file to Jupyter Notebook.

--- src.py
+++
@@ -1,9 +0,0 @@
-import nfl_data_py as nfl 
-
-
-years = [2018, 2019, 2020]
-
-
-qbr = nfl.import_qbr(years = years, level = 'nfl', frequency = 'season')
-
-print(qbr.head())
---
+++ test.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np 
+import seaborn as sns
+import nba_api as nba
+
+print('Testing Script')
